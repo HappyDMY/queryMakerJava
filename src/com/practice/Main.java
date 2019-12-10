f38ef27e9bb124a07e56c0f147820f316584314a
jun <vagrantsj@gmail.com>
2019-12-10 13:07:55 +0900

    상황에 따른 select 문 생성

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -5,64 +5,185 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+public class Main<Static> {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
+//	    String fileName1 = "E:\\NCS\\신안산대\\SECTION_CD.txt";
+//		getSectinoQuery(fileName1);
+//
+//	    String fileName2 = "E:\\NCS\\신안산대\\SUB_SECTION_CD.txt";
+//		getSubSectinoQuery(fileName2);
 
-        File file = new File("E:\\NCS_ncsClCdsForQuery.txt");
-        //입력 스트림 생성
-        BufferedReader filereader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+	    String fileName3 = "E:\\NCS\\신안산대\\ncsComptUnitNos.txt";
+		getExtraQeury(fileName3);
 
-        StringBuilder textSb = new StringBuilder();
-        int singleCh = 0;
-        while ((singleCh = filereader.read()) != -1) {
+	}
+	public Main(){}
+
+    public static void getSubSectinoQuery(String fileName) {
+	    File file = new File(fileName);
+	    //입력 스트림 생성
+	    BufferedReader filereader = null;
+	    try {
+		    filereader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+
+
+		    StringBuilder textSb = new StringBuilder();
+		    int singleCh = 0;
+		    while ((singleCh = filereader.read()) != -1) {
 //				System.out.print((char)singleCh);
-            textSb.append((char) singleCh);
-        }
-        filereader.close();
+			    textSb.append((char) singleCh);
+		    }
+		    filereader.close();
+
+		    List<String> rowDataList = new ArrayList();
+		    rowDataList = Arrays.asList(textSb.toString().replace("\r", "").split("\n"));
+		    String data = "";
 
 
-        List<String> NCS_ncsClCds = new ArrayList();
-        NCS_ncsClCds = Arrays.asList(textSb.toString().replace("\r", "").split("\n"));
-//			Map jsonResutl = api.getResult("0201020103_18v2");
-        String data = "";
+		    System.out.println("--세분류 insert해야할 목록 조회 쿼리------------");
 
 
-        List tb = new ArrayList();
 
-        tb.add("NCBT100");
-        tb.add("NCBT110");
-        tb.add("NCBT115");
-        tb.add("NCBT110");
-        tb.add("NCBT120");
-        tb.add("NCBT130");
-        tb.add("NCBT140");
-        tb.add("NCBT150");
-        tb.add("NCBT160");
-        tb.add("NCBT170");
-        tb.add("NCBT180");
+		    for (Object line : rowDataList) {
+			    String a = line.toString().substring(0,2);
+			    String b = line.toString().substring(2,4);
+			    String c = line.toString().substring(4,6);
+			    String d = line.toString().substring(6,8);
 
-        for (Object line : NCS_ncsClCds) {
-            for (Object table : tb) {
-                data = line.toString().trim();
-//				System.out.println("SELECT * FROM " + table + " WHERE COMPT_UNIT_NO='" + data + "';");
-                System.out.println("Delete FROM " + table + " WHERE COMPT_UNIT_NO='" + data + "';");
-            }
-            System.out.println();
+			    if (rowDataList.indexOf(line)==rowDataList.size()-1){
+				    System.out.println("SELECT * FROM NCBT040 WHERE NCS_DIV='N'AND CATEGORY_CD='"+a+"' AND DIVISION_CD='"+b+"' AND SECTION_CD='"+c+"' AND SUB_SECTION_CD='"+d+"'");
 
-//        }
-//
-//        for (Object table : tb) {
-//            System.out.println("Delete FROM " + table + " WHERE COMPT_UNIT_NO in(");
-//            for (Object line : NCS_ncsClCds) {
-//                data = line.toString().trim();
-//				System.out.println("'data',");
-//            }
-//
-//            System.out.println(")");
-//            System.out.println();
+			    }else{
+				    System.out.println("SELECT * FROM NCBT040 WHERE NCS_DIV='N'AND CATEGORY_CD='"+a+"' AND DIVISION_CD='"+b+"' AND SECTION_CD='"+c+"' AND SUB_SECTION_CD='"+d+"' UNION ALL");
+			    }
 
-        }
 
-    }
+
+		    }
+
+	    } catch (UnsupportedEncodingException e) {
+		    e.printStackTrace();
+	    } catch (IOException e) {
+		    e.printStackTrace();
+	    }
+
+	}
+
+    public static void getSectinoQuery(String fileName) {
+		File file = new File(fileName);
+		//입력 스트림 생성
+		BufferedReader filereader = null;
+		try {
+			filereader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+
+
+			StringBuilder textSb = new StringBuilder();
+			int singleCh = 0;
+			while ((singleCh = filereader.read()) != -1) {
+//				System.out.print((char)singleCh);
+				textSb.append((char) singleCh);
+			}
+			filereader.close();
+
+			List<String> rowDataList = new ArrayList();
+			rowDataList = Arrays.asList(textSb.toString().replace("\r", "").split("\n"));
+			String data = "";
+
+			System.out.println("--소분류 insert해야할 목록 조회 쿼리------------");
+
+
+
+			for (Object line : rowDataList) {
+				String a = line.toString().substring(0,2);
+				String b = line.toString().substring(2,4);
+				String c = line.toString().substring(4,6);
+
+				if (rowDataList.indexOf(line)==rowDataList.size()-1){
+					System.out.println("SELECT * FROM NCBT030 WHERE NCS_DIV='N'AND CATEGORY_CD='"+a+"' AND DIVISION_CD='"+b+"' AND SECTION_CD='"+c+"'");
+
+				}else{
+					System.out.println("SELECT * FROM NCBT030 WHERE NCS_DIV='N'AND CATEGORY_CD='"+a+"' AND DIVISION_CD='"+b+"' AND SECTION_CD='"+c+"' UNION ALL");
+				}
+
+
+
+			}
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+    public static void getExtraQeury(String fileName) {
+		File file = new File(fileName);
+		//입력 스트림 생성
+		BufferedReader filereader = null;
+		try {
+			filereader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+
+
+			StringBuilder textSb = new StringBuilder();
+			int singleCh = 0;
+			while ((singleCh = filereader.read()) != -1) {
+//				System.out.print((char)singleCh);
+				textSb.append((char) singleCh);
+			}
+			filereader.close();
+
+			List<String> rowDataList = new ArrayList();
+			rowDataList = Arrays.asList(textSb.toString().replace("\r", "").split("\n"));
+			String data = "";
+
+
+
+			List tb = new ArrayList();
+
+			tb.add("NCBT100");
+			tb.add("NCBT110");
+			tb.add("NCBT115");
+			tb.add("NCBT110");
+			tb.add("NCBT120");
+			tb.add("NCBT130");
+			tb.add("NCBT140");
+			tb.add("NCBT150");
+			tb.add("NCBT160");
+			tb.add("NCBT170");
+			tb.add("NCBT180");
+
+			for (Object tbnm :tb){
+				System.out.println("-- insert해야할 ["+tbnm+"] 목록 조회 쿼리------------");
+				System.out.println("SELECT * FROM "+tbnm+" WHERE NCS_DIV='N' AND COMPT_UNIT_NO IN (");
+				for (Object line : rowDataList) {
+
+					if (rowDataList.indexOf(line)==rowDataList.size()-1){
+						System.out.print("'"+line+"'");
+					}else if(rowDataList.indexOf(line)==700) {
+						System.out.println("'"+line+"'");
+						System.out.print(") or COMPT_UNIT_NO IN (");
+
+					}else{
+						System.out.print("'"+line+"', ");
+					}
+
+				}
+				System.out.println(");");
+				System.out.println();
+			}
+
+
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
 }
